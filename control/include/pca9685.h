f5d8f47e1d3251264a5b60b741d90c502689d41f
AIAAOOAO <159702851+temesotejam@users.noreply.github.com>
2026-08-06 13:42:42 +0900

    Add per-channel PCA9685 full-off control

--- control/include/pca9685.h
+++ control/include/pca9685.h
@@ -1,10 +1,29 @@
 #pragma once
 #include <Arduino.h>
-struct PcaChannel { uint16_t on=0, off=0; bool fullOff=true; };
+
+struct PcaChannel {
+  uint16_t on = 0;
+  uint16_t off = 0;
+  bool fullOff = false;
+};
+
 class Pca9685 {
  public:
-  bool begin(); bool allOff(); bool setPulse(uint8_t channel,uint16_t pulseUs,PcaChannel* actual=nullptr); bool readChannel(uint8_t channel,PcaChannel& out);
-  bool detected=false; uint8_t mode1=0,mode2=0,prescale=0; uint32_t errors=0,readbackErrors=0; float actualHz=0;
+  bool begin();
+  bool allOff();
+  bool setFullOff(uint8_t channel);
+  bool setPulse(uint8_t channel, uint16_t pulseUs, PcaChannel* actual = nullptr);
+  bool readChannel(uint8_t channel, PcaChannel& output);
+  bool detected = false;
+  uint8_t mode1 = 0;
+  uint8_t mode2 = 0;
+  uint8_t prescale = 0;
+  float actualHz = 0;
+  uint32_t errors = 0;
+  uint32_t readbackErrors = 0;
  private:
-  bool write8(uint8_t reg,uint8_t value); bool writeN(uint8_t reg,const uint8_t* data,size_t count); bool read8(uint8_t reg,uint8_t& value); bool ack();
+  bool ack();
+  bool write8(uint8_t reg, uint8_t value);
+  bool writeN(uint8_t reg, const uint8_t* data, size_t length);
+  bool read8(uint8_t reg, uint8_t& value);
 };
